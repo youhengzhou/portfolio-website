d216c662b04922318b18bcc0e6b427f7f11981c1
DESKTOP-I1B98FM\YouHengZhou <youhengzhou@cmail.carleton.ca>
2022-11-11 17:17:35 -0500

    page title

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import { Container, Box, Heading } from "@chakra-ui/react";
+
 const Page = () => {
-    return <div>h world </div>;
+    return (
+        <Container>
+            <Box borderRadius="lg" bg="red" p={3} align="center">
+                Hello, my name is Yohannes, and I am a fulltime software
+                developer, part time open-source Python package maintainer.
+            </Box>
+
+            <Box display={{ md: "flex" }}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title"></Heading>
+                </Box>
+            </Box>
+        </Container>
+    );
 };
 
 export default Page;
